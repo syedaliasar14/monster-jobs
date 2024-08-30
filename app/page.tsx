df8980d8ad6ba0eb5aa10e5edac1002016d4a585
Syed Ali Asar <sa.asar14@gmail.com>
2024-08-30 18:00:00 -0400

    setup clerk & convex

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
+"use client";
+
+import { api } from "@/convex/_generated/api";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { useQuery } from "convex/react";
 
 export default function WelcomePage() {
+
+  const tasks = useQuery(api.tasks.get)
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div>Welcome to</div>
       <div>Monster Jobs</div>
+      <SignedIn>
+        <button>Register a Monster Employee</button>
+        <button>View all Employees</button>
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
     </main>
   );
 }
